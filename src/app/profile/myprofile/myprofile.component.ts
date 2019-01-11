854567c5add3a1c45aa7f5649663e62ca39f345f
Minh <minhtt262@gmail.com>
2019-01-11 15:36:11 +0700

    profile

--- src/app/profile/myprofile/myprofile.component.ts
+++ src/app/profile/myprofile/myprofile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../share/services/user.service';
 import { ArticleService } from '../../share/services/article.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
@@ -10,19 +11,25 @@ export class MyprofileComponent implements OnInit {
   lists: any;
   public check : boolean = true;
   user = localStorage.getItem('user');
-  author = localStorage.getItem('author');
+  //author = localStorage.getItem('author');
   // params:any = {};
+  author: string;
 
-  constructor(private article: ArticleService) { }
+  constructor(private article: ArticleService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     //this.myarticlelist();
     //this.profile();
     //localStorage.removeItem('author')
     this.abc();
-    localStorage.removeItem('author');
+    //localStorage.removeItem('author');
   }
+
   public abc(){
+    this.route.params.subscribe((params) => {
+      this.author = params['id'];
+       console.log(this.author);
+    });
     if(this.author == null){
         this.profile();
     }else{
